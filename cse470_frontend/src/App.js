844ae43cd8e15095d9b05be1688daffed4446775
mostofa-al-maruf <mahmudmaruf1969@gmail.com>
2024-12-26 20:01:27 +0600

    feat: add admin login

--- cse470_frontend/src/App.js
+++ cse470_frontend/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link, useNavigate } from 'react-router-dom';
 import Slideshow from './slide';
 import './App.css';
 import Login from './login'; // Import your Login component
 import Signup from './signup'; // Import your Signup component
-
+import LoginOptions from './LoginOptions'; // Import LoginOptions component
+import AdminLogin from './AdminLogin';
 function App() {
     return (
         <Router>
@@ -15,6 +16,10 @@ function App() {
                 <Route path="/login" element={<Login />} />
                 {/* Route for the signup page */}
                 <Route path="/signup" element={<Signup />} />
+                {/* Route for the login options page */}
+                <Route path="/login-options" element={<LoginOptions />} />
+                {/* Route for the admin login options page */}
+                <Route path="/admin-login" element={<AdminLogin />} />
             </Routes>
         </Router>
     );
@@ -22,11 +27,17 @@ function App() {
 
 // HomePage component: contains the homepage content
 function HomePage() {
+    const navigate = useNavigate();
+
+    const handleStaffLogin = () => {
+        navigate('/login-options'); // Redirect to LoginOptions component
+    };
+
     return (
         <div className="App">
             <header>
                 <h1>Welcome to xyz Cafeteria</h1>
-                <button className="staff-login">Staff Login</button>
+                <button className="staff-login" onClick={handleStaffLogin}>Staff Login</button>
             </header>
             <Slideshow />
             <h1>
@@ -34,7 +45,6 @@ function HomePage() {
                     <button>Want to order something?</button>
                 </Link>
             </h1>
-            {/* Removed the signup button */}
         </div>
     );
 }
