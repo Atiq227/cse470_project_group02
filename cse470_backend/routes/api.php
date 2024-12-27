70bf56f8a451c2c3790449073b9d10aca9fbfbdb
Mahmuda844 <mahmuda.tabassum@g.bracu.ac.bd>
2024-12-27 09:38:07 +0600

    chef-addmenu-addfood-removefood-acceptorder-declineorder-sendnotification done with backend

--- cse470_backend/routes/api.php
+++ cse470_backend/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ChefController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -18,3 +19,12 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::post('register', [UserController::class, 'register']);
+
+Route::post('/menu/add', [ChefController::class, 'addMenu']);
+Route::post('/menu/item/add', [ChefController::class, 'addFoodItem']);
+Route::delete('/menu/item/remove/{id}', [ChefController::class, 'removeFoodItem']);
+Route::post('/order/{id}/accept', [ChefController::class, 'acceptOrder']);
+Route::post('/order/{id}/decline', [ChefController::class, 'declineOrder']);
+Route::post('/notify', [ChefController::class, 'sendNotification']);
+Route::get('/homepage', [ChefController::class, 'homepage']);
+
